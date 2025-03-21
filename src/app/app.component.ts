1d38b5951b3ccda9fe6186477864d59e4fa9ae36
Jacques <jacquesvanluyck@gmail.com>
2025-03-21 11:04:35 +0100

    update filter-control.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
+import { FilterControlsComponent } from "./filter-controls/filter-controls.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, FilterControlsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/filter-controls/filter-controls.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-controls',
+  imports: [],
+  templateUrl: './filter-controls.component.html',
+  styleUrl: './filter-controls.component.css'
+})
+export class FilterControlsComponent {
+
+}
