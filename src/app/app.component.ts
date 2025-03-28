e8e42178f78849a76f5b328e8a462d15e462152c
Jacques <jacquesvanluyck@gmail.com>
2025-03-28 13:57:10 +0100

    update new-task component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./navigation/navigation.component";
 import { FilterControlsComponent } from "./filter-controls/filter-controls.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavigationComponent, FilterControlsComponent],
+  imports: [NavigationComponent, FilterControlsComponent, NewTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
